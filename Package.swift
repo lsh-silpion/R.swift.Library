0c7772e007ef8ae4de71be2cc9446481b75568e9
Lars Sonchocky-Helldorf <sonchocky-helldorf@silpion.de>
2024-02-13 21:02:58 +0100

    make project build again

--- Package.swift
+++ Package.swift
@@ -5,9 +5,9 @@ import PackageDescription
 let package = Package(
     name: "R.swift.Library",
     platforms: [
-        .iOS(.v9),
-        .tvOS(.v9),
-        .watchOS(.v2),
+        .iOS(.v12),
+        .tvOS(.v12),
+        .watchOS(.v5),
     ],
     products: [
         .library(name: "Rswift", targets: ["Rswift"]),
